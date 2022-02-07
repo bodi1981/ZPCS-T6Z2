bb731ef4af0b1b9853369d90482e71633a91d920
Wojtek <wojtek.bogdziun@gmail.com>
2022-02-07 19:36:49 +0100

    Add project files.

---
+++ T6Z2/Main.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace T6Z2
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbPicture = new System.Windows.Forms.PictureBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbPicture
+            // 
+            this.pbPicture.Location = new System.Drawing.Point(6, 19);
+            this.pbPicture.Name = "pbPicture";
+            this.pbPicture.Size = new System.Drawing.Size(409, 334);
+            this.pbPicture.TabIndex = 0;
+            this.pbPicture.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Beige;
+            this.btnAdd.Location = new System.Drawing.Point(38, 78);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 44);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Dodaj zdjęcie";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.btnRemove.Location = new System.Drawing.Point(38, 138);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(100, 44);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Usuń zdjęcie";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Visible = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbPicture);
+            this.groupBox1.Location = new System.Drawing.Point(176, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 361);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Obrazek";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(608, 384);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Main";
+            this.Text = "Przęglądarka zdjęć";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPicture;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ T6Z2/Main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using T6Z2.Properties;
+
+namespace T6Z2
+{
+    public partial class Main : Form
+    {
+        public string PicturePath
+        {
+            get
+            {
+                return Settings.Default.PicturePath;
+            }
+            set
+            {
+                Settings.Default.PicturePath = value;
+            }
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+
+            if (PicturePath != "")
+            {
+                pbPicture.Image = new Bitmap(PicturePath);
+                pbPicture.SizeMode = PictureBoxSizeMode.Zoom;
+                btnRemove.Visible = true;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "Image Files|*.gif;*.jpg;*.jpeg;*.png";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var path = dialog.FileName;                     //zmienna path powstala w celu aby w przypadku zaczytania 
+                        pbPicture.Image = new Bitmap(path);             //nieprawidlowego pliku nie przypisac blednej sciezki do 
+                        pbPicture.SizeMode = PictureBoxSizeMode.Zoom;   //zmiennej PicturePath
+                        PicturePath = path;
+                        btnRemove.Visible = true;
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Wybrany plik nie jest prawidłowym plikiem graficznym", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            pbPicture.Image = null;
+            btnRemove.Visible = false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (pbPicture == null || pbPicture.Image == null)
+                PicturePath = "";
+
+            Settings.Default.Save();
+        }
+    }
+}
